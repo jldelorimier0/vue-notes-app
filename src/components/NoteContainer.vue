a07c0a6992f4968931d1e8160de3a4ddd434007a
Julia de L <jldelorimier@gmail.com>
2020-04-27 21:27:30 -0400

    put Toolbar, NoteContainer, and NoteSelectors into their own files in Components. added import/export lines so they render together

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="note-container">
+  <!-- note-selectors goes here -->
+  <note-selectors />
+    <div class="note-editor">
+      <p class="note-editor-info">Timestamp here...</p>
+      <textarea class="note-editor-input">
+        First note...
+        Note text here...
+      </textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+import NoteSelectors from "@/components/NoteSelectors"
+
+  export default {
+    name: 'note-container',
+    components: {
+      NoteSelectors
+    }
+  }
+</script>
